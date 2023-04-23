42c6cd0054df5bd5db406d5f308ff4d7ef2a0b3d
AlexCorn999 <alexcorn999@list.ru>
2023-04-23 18:08:37 +0300

    added database

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/AlexCorn999/http-rest-api-new/internal/app/store"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -13,6 +14,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New ...
@@ -31,6 +33,10 @@ func (s *APIServer) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 	s.logger.Info("starting api server")
 
 	return http.ListenAndServe(s.config.BindAddr, s.router)
@@ -42,6 +48,17 @@ func (s *APIServer) configureLogger() error {
 		return err
 	}
 	s.logger.SetLevel(level)
+	return nil
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
 	return nil
 }
 
